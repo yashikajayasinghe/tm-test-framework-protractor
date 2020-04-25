9f24065a5f828581521722e9787a044a01d69c8a
yjayasinghe <sam@example.comyashika.jayasinghe@trademe.co.nz>
2020-04-25 23:14:20 +1200

    ElemenArrayFinder and test2 with async wait

--- page_objects/tm-mp-search-po.ts
+++ page_objects/tm-mp-search-po.ts
@@ -1,11 +1,14 @@
-import { ElementFinder, element, by } from "protractor";
+import { ElementFinder, ElementArrayFinder, element, by } from "protractor";
 
 export class tmMPSearch{
 
     catSugesstionHeading:ElementFinder;
+    allListingCards:ElementArrayFinder;
+    
 
     constructor(){
         this.catSugesstionHeading = element(by.css('tm-category-suggestions h2[class="tm-category-suggestions__heading"]'));
+        this.allListingCards = element.all(by.css('tm-search-results tm-search-card-switcher'));
 
     }
 
--- tm-spec.ts
+++ tm-spec.ts
@@ -16,7 +16,9 @@ describe('As a TM user I can got to TM main Homepage to perform a search', funct
 	it('go to tm home and search for a baby stroller', async () => {
 
 		await browser.get("https://www.trademe.co.nz/a"); //Fact: Homepage is identified as an agnular page by the protractor webdriver
+		
 		let homepage = new tmHomePo();
+		
 		await homepage.searchTextBox.sendKeys('baby stroller');
 
 		browser.waitForAngularEnabled(false); //Hack for: the search results page is not being identified as an angular page	
@@ -24,11 +26,49 @@ describe('As a TM user I can got to TM main Homepage to perform a search', funct
 		await homepage.searchButton.click()
 
 		let MpSearch = new tmMPSearch();
+		
 		let EC = browser.ExpectedConditions;
-		await browser.wait(EC.visibilityOf(MpSearch.catSugesstionHeading), 5000, 'waiting for the dialog title to appear');    
-    	expect(MpSearch.catSugesstionHeading.getText()).toContain("Narrow your search for 'baby stroller'");
+		
+		await browser.wait(EC.visibilityOf(MpSearch.catSugesstionHeading), 5000, 'waiting for the category sugesstion header to appear');    
+		
+		console.log((await MpSearch.allListingCards).length);
+		
+		expect(MpSearch.catSugesstionHeading.getText()).toContain("Narrow your search for 'baby stroller'");		
 
 	});
+	/**
+	 * Protractor : actions API
+	 */
+
+	it('go to tm home and search for samsung and select search sugesstions', async () => {
+		
+		await browser.get("https://www.trademe.co.nz/a");
+
+		let homepage = new tmHomePo();
+
+		let EC1 = browser.ExpectedConditions;
+
+		await homepage.searchTextBox.sendKeys('samsung');
+
+		await browser.wait(EC1.visibilityOf(homepage.searchSugesstions), 3000, 'Waiting for search suggestions to appear')
+
+		await browser.actions().sendKeys(protractor.Key.ARROW_DOWN).perform();
+
+		browser.waitForAngularEnabled(false); //Hack for: the search results page is not being identified as an angular page
+
+		await browser.actions().sendKeys(protractor.Key.ENTER).perform();
+
+		let MpSearch = new tmMPSearch();
+
+		await browser.wait(EC1.urlContains('https://www.trademe.co.nz/a/marketplace/mobile-phones/mobile-phones/samsung'), 5000, 'waiting for the url to change');
+
+		let SearchResults = await MpSearch.allListingCards;
+
+		expect(SearchResults.length).toBe(24);
+
+
+
+	});	
 
 	
 });
